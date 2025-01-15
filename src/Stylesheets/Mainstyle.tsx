8f35db1e3f3201ef6ac9d780e4b38c94c6d55f36
ankitojha07 <ankitojha1409@gmail.com>
2025-01-15 23:59:28 +0530

    splash screen added

---
+++ src/Stylesheets/Mainstyle.tsx
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  logoStyle: {
+    width: 300,
+    height: 300,
+  },
+  container: {
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+export default styles;
--- src/pages/static/Splashscreen.tsx
+++ src/pages/static/Splashscreen.tsx
@@ -1,28 +1,15 @@
-import {StyleSheet, View} from 'react-native';
+import {View} from 'react-native';
 import React from 'react';
 import {Image} from 'react-native-elements';
 import logo from '../../../assets/images/logo.png';
+import styles from '../../Stylesheets/Mainstyle.tsx';
 
 const Splashscreen = () => {
   return (
     <View style={styles.container}>
-      <Image source={logo} style={styles.logoStyle} />
+      <Image source={logo} style={styles.logoStyle} />dded
     </View>
   );
 };
 
 export default Splashscreen;
-
-const styles = StyleSheet.create({
-  logoStyle: {
-    width: 300,
-    height: 300,
-  },
-  container: {
-    height: '100%',
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
